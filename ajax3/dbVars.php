8fd9f5893037efc244498eac242e7bb4e61e785e
Taurese <43673192+25487@users.noreply.github.com>
2019-04-08 20:12:23 +0200

    Add files via upload

---
+++ ajax3/dbVars.php
@@ -0,0 +1,6 @@
+<?php
+  $servername = "127.0.0.1";
+  $username   = "taurese02";
+  $password   = "7EYNVSLcCf7ufh1F";
+  $database   = "samplevideo_db";
+?>
---
+++ ajax3/getdata.php
@@ -0,0 +1,36 @@
+<?php
+  include "dbVars.php";
+  $debug = false;
+  $q     = $_GET['q'];
+  $q     = filter_var($q, FILTER_SANITIZE_STRING);
+  $con   = mysqli_connect($servername, $username, $password, $database);
+  if(!$con){
+    die('Could not connect: ' . mysqli_error($con));
+  }
+  $sql   = "SELECT * FROM user_details WHERE last_name LIKE '$q%'";
+  if($debug){echo $sql;} //debug
+  $result = mysqli_query($con, $sql);
+  if (mysqli_num_rows($result) == 0){
+    echo "<p>Geen resultaat</p>";
+  }else{
+  echo "<table>
+  <tr>
+  <th>Firstname</th>
+  <th>Lastname</th>
+  <th>Gender</th>
+  <th>UID</th>
+  <th>Password</th>
+  </tr>";
+  while($row = mysqli_fetch_array($result)){
+    echo "<tr>";
+    echo "<td>" . $row['first_name'] . "</td>";
+    echo "<td>" . $row['last_name'] . "</td>";
+    echo "<td>" . $row['gender'] . "</td>";
+    echo "<td>" . $row['user_id'] . "</td>";
+    echo "<td>" . $row['password'] . "</td>";
+    echo "</tr>";
+  }
+  echo "</table>";
+  }
+  mysqli_close($con);
+?>
